23b1732b019248818042ed9eb45ca99ce28ff326
Laxman Chamlagai <laxmanchamlagai20@gmail.com>
2019-08-14 01:49:14 -0400

    string project is added

---
+++ String_project/src/com/JavaLearning/Main.java
@@ -0,0 +1,22 @@
+package com.JavaLearning;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// 8 primitive java data types
+        // byte
+        // int
+        //short
+        //float
+        // long
+        // char
+        // double
+        // boolean
+        String myString = "This is my String";
+        System.out.println("My String value is = " + myString);
+        myString = myString  +  ".. ...this is more string added here";
+        System.out.println(myString);
+        myString = myString  + " \u00a92019";
+        System.out.println(myString);
+    }
+}
